d273cc71935897191cd7ba8cc3c5879ed20a3338
alex-salnikov <alexander.salnikov@gmail.com>
2024-05-21 17:07:40 +0000

    start-script for replit

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Setup
+
+## install nvm, node
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# nvm install --lts
+
+## apply profile (replit)
+# source ~/.profile
+
+node index.js
